e42d8e915b85c4e2d5c24dbc58e88a427196110e
Adejaime Jr <adejaime88@gmail.com>
2023-03-20 22:02:53 -0400

    link paginas

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 function Home() {
-    return  <h2>Home</h2>
+    return  (
+        <>
+            <h2>Home</h2>
+            <Link href="/sobre">
+                Acessar página Sobre
+            </Link>
+        </>
+    )
 };
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+function Sobre() {
+    return  (
+        <>
+            <h2>Sobre</h2>
+            <Link href="/">
+                Acessar página Home
+            </Link>
+        </>
+    )
+};
+
+export default Sobre
